e6dab44db052868206de3b9fdcc53b31093d61ab
Muhammed Khalid <mohamedkhaled303110@gmail.com>
2022-03-10 22:47:32 +0200

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.task.app.id',
+  appName: 'product-viewer',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
